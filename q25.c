5076df5077e176ac3d6bbf241727ea79a0f05b75
lucky21-step <Lucky.33894@stu.upes.ac.in>
2026-08-22 21:22:18 +0530

    Add q25 program

---
+++ q25.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{
+int a, b;
+char op;
+
+scanf("%d %d %c", &a, &b, &op);
+
+switch (op)
+{
+case '+':
+printf("%d", a + b);
+break;
+
+case '-':
+printf("%d", a - b);
+break;
+
+case '*':
+printf("%d", a * b);
+break;
+
+case '/':
+printf("%d",a / b);
+break;
+
+case'%':
+printf("%d", a % b);
+break;
+}
+return 0;
+}
