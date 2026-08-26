0cebada1ef8aa1c6534ed1167fe6b301ea0eacaf
lucky21-step <Lucky.33894@stu.upes.ac.in>
2026-08-26 09:05:42 +0530

    add q33 q34 program

---
+++ q33.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+int n, temp, digit, sum = 0;
+ scanf("%d", &n);
+
+temp = n;
+
+while (temp != 0)
+{
+digit = temp % 10;
+sum = sum + digit * digit * digit;
+temp = temp / 10;
+}
+if (sum == n)
+printf("Armstrong");
+else
+printf("Not Arstrong");
+return 0;
+}
