4b566c74f73bc46871dd1946fbdc8bd4fabbe7b3
lucky21-step <Lucky.33894@stu.upes.ac.in>
2026-09-04 20:30:41 +0530

    add q51 q52 program

---
+++ q52.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int i, j;
+for(i = 1; i <= 5; i++)
+{
+int stars;
+if (i <= 3)
+stars = 2 * i - 1;
+else
+stars = 2 * (5 - i) + 1;
+for (j = 1; j <= stars; j++)
+{
+printf("*\n");
+}
+printf("\n");
+}
+return 0;
+}
