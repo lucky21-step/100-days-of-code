a8a9239e9ed22b2f87ac53f814f75e1772fdf423
lucky21-step <Lucky.33894@stu.upes.ac.in>
2026-09-06 00:33:06 +0530

    Add q55 q56 program

---
+++ q56.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+int n, i;
+int arr[100];
+
+printf("Enter number of elements: ");
+scanf("%d", &n);
+
+for (i = 0; i < n; i++)
+{
+scanf("%d", &arr[i]);
+}
+for (i = 0; i < n; i++)
+{
+printf("%d",arr[i]);
+}
+return 0;
+}
