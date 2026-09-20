8960f648fe5f2a9f5b16fbc7652f9d31fc22fb36
Lucky <Lucky.33894@stu.upes.ac.in>
2026-09-20 23:00:35 +0530

    Add q83 q84 program

---
+++ q83.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int vowels = 0, consonants = 0;
+
+    scanf("%99s", str);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        char ch = str[i];
+
+        if (ch >= 'A' && ch <= 'Z') {
+            ch = ch + 32;
+        }
+
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' ||
+                ch == 'o' || ch == 'u') {
+                vowels++;
+            } else {
+                consonants++;
+            }
+        }
+    }
+
+    printf("Vowels=%d, Consonants=%d", vowels, consonants);
+
+    return 0;
+}
